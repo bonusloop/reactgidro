de408facdd8407996d5e757ed29744be386a2e34
bonusloop <denis7sined@gmail.com>
2021-09-29 19:23:45 +0300

    functionTest

--- src/App.js
+++ src/App.js
@@ -1,14 +1,41 @@
 import './App.css';
 
-function App() {
+const App = () => {
   return (
+    <div>
+      <Header/>
     <div className="App">
-      
-      
-          React test shmall 
-      
+      <ul>
+        <li>
+          css
+        </li>
+        <li>html</li>
+        <li>js</li>
+        <li>react</li>
+      </ul>
     </div>
+      <Fix/>
+    </div>
+
   );
+}
+
+const Header = () => {
+  return (
+    <div>
+     <a href="https://pzz.by/">Top</a>
+     <a href="https://dominos.by/">News</a>
+     <a href="https://dodopizza.by/minsk">Home</a>
+    </div>
+  )
+}
+
+const Fix = () => {
+  return (
+    <div>
+      <h1>Ёлупукенен</h1>
+    </div>
+  )
 }
 
 export default App;
